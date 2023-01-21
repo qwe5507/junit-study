41b70d7c601068e6c3ca44f3642c648ac865d2e6
qwe5507 <apvmffkdls@gmail.com>
2023-01-21 14:05:17 +0900

    서비스레이어 테스트 - 실제 db로 테스트

--- src/main/java/com/example/junit/dto/BookSaveReqDto.java
+++ src/main/java/com/example/junit/dto/BookSaveReqDto.java
@@ -1,11 +1,13 @@
 package com.example.junit.dto;
 
 import com.example.junit.domain.Book;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter // Controller에서 Setter가 호출되면서 DTO에 값이 세팅
+@AllArgsConstructor
 public class BookSaveReqDto {
     private String title;
     private String author;
--- src/test/java/com/example/junit/service/BookServiceTest.java
+++ src/test/java/com/example/junit/service/BookServiceTest.java
@@ -1,4 +1,33 @@
 package com.example.junit.service;
 
+import com.example.junit.domain.BookRepository;
+import com.example.junit.dto.BookResponseDto;
+import com.example.junit.dto.BookSaveReqDto;
+import com.example.junit.util.MailSenderStub;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class BookServiceTest {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Test
+    public void 책등록하기_테스트() {
+        // given
+        BookSaveReqDto dto = new BookSaveReqDto("junit", "메타코딩");
+        // stub
+        MailSenderStub mailSenderStub = new MailSenderStub();
+
+        // when
+        BookService bookService = new BookService(bookRepository, mailSenderStub);
+        BookResponseDto bookResponseDto = bookService.책등록하기(dto);
+
+        // then
+        Assertions.assertEquals(dto.getTitle(), bookResponseDto.getTitle());
+        Assertions.assertEquals(dto.getAuthor(), bookResponseDto.getAuthor());
+    }
 }
